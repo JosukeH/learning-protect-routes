65c8b6890d69fa40d56bd164fc481e510e4f6c86
JosukeH <solodcarlos@gmai.com>
2022-04-07 17:50:54 -0500

    make it better

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const Auth = () => {
+  return (
+    <>
+    <div>Auth</div>
+    <Link> ir al otro Router</Link>
+    </>
+  )
+}
+
+export default Auth
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <>
+    <h1>Contact</h1>
+    </>
+  )
+}
+
+export default Home
